ea37a124bcf208f797985077566e65a293c1f373
Lakshmipriya M <priyam271004@gmail.com>
2025-06-19 19:08:47 +0530

    Update scriptt.js

--- scriptt.js
+++ scriptt.js
@@ -1,4 +1,16 @@
-document.querySelector('.back-to-top').addEventListener('click', function(e) {
+// Show "Back to Top" button on scroll
+const backToTopBtn = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        backToTopBtn.style.display = 'block';
+    } else {
+        backToTopBtn.style.display = 'none';
+    }
+});
+
+// Smooth scroll to top
+backToTopBtn.addEventListener('click', (e) => {
     e.preventDefault();
     window.scrollTo({
         top: 0,
